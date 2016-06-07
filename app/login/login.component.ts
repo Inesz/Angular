d162d1cf99b3d054dcf1640343c81966a7d417fc
Inesz <agnieszka-garsta@wp.pl>
2016-06-07 19:13:35 +0200

    form validation

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm }    from '@angular/common';
 import { OnActivate, Router, RouteSegment } from '@angular/router';
 import { bootstrap }        from '@angular/platform-browser-dynamic';
 import { ROUTER_PROVIDERS } from '@angular/router';
@@ -15,28 +16,30 @@ export class LoginComponent implements OnInit {
 constructor(private loginService: LoginService, public mainService : MainService, private router:Router) { }
     
     public login : string = "";
-    public msgNotFree: string = "";
-    
+    public ifLoginFree : boolean = true;
+
     ngOnInit() { }
     
+    private submitted = false;
+    onSubmit() { 
+    console.log("submit");
+    this.submitted = true; }
+    
     checkLogin(){
+        this.ifLoginFree = true;
+        
         this.loginService.checkLogin(this.login).subscribe(ifFree => {
             console.log(ifFree);
             
         if (ifFree === 0){
             this.mainService.setLogin(this.login);
-        console.log( this.mainService.getLogin());
-            this.msgNotFree = "";
-            this.login = "";
+            console.log( this.mainService.getLogin());
             this.router.navigate(['/games']);
         }else{
             this.login = "";
-            this.msgNotFree = "login zajęty";
+            this.ifLoginFree = false;
         }
         });
 
     }
 }
-
-
-
